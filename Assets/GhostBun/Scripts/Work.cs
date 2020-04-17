76b8f54667b20328c976276b6c1db492b1e6a019
Bezymyanyj <Bezymyanyj@gmail.com>
2020-04-16 20:38:04 -0400

    AI Animations

--- Assets/GhostBun/Scripts/Work.cs
+++ Assets/GhostBun/Scripts/Work.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EmeraldAI;
 
 public class Work : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("I work");
+        other.GetComponent<EmeraldAIEventsManager>().PlayEmoteAnimation(0);
+            
     }
 }
