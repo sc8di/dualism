e0492e8de1742f10feeac89744f5889f4f96bd66
Bezymyanyj <Bezymyanyj@gmail.com>
2020-04-27 16:24:32 -0400

    Update Work.cs
    Add Timer disable

--- Assets/GhostBun/Scripts/Work.cs
+++ Assets/GhostBun/Scripts/Work.cs
@@ -8,18 +8,27 @@ public class Work : MonoBehaviour
 
     private EmeraldAIEventsManager _eventSystem;
 
+    public float turnOffTrigetTime = 10f;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Worker on point: " + _emoteAnimationIndex);
+        //Debug.Log("Worker on point: " + _emoteAnimationIndex);
         if (other.CompareTag("NPC"))
         {
-            Debug.Log("Worker on point: " + _emoteAnimationIndex);
+            //Debug.Log("Worker on point: " + _emoteAnimationIndex);
             _eventSystem = other.GetComponent<EmeraldAIEventsManager>();
 
             // Останавливаем движение NPC до окончания анимации
             StartCoroutine(Working(other.GetComponent<AnimationsArray>().AnimationsLength[_emoteAnimationIndex]));
         }
-        
+        else if (other.CompareTag("Player"))
+        {
+            //Debug.Log("Worker on point: " + _emoteAnimationIndex);
+            _eventSystem = other.GetComponent<EmeraldAIEventsManager>();
+
+            // Останавливаем движение NPC до окончания анимации
+            StartCoroutine(Working(other.GetComponent<AnimationsArray>().AnimationsLength[_emoteAnimationIndex]));
+        }
+
     }
 
     private IEnumerator Working(float delay)
@@ -28,5 +37,14 @@ public class Work : MonoBehaviour
         _eventSystem.PlayEmoteAnimation(_emoteAnimationIndex);
         yield return new WaitForSeconds(delay);
         _eventSystem.ResumeMovement();
+        StartCoroutine(TurnOffCollider(turnOffTrigetTime));
+    }
+
+
+    private IEnumerator TurnOffCollider(float delay)
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(delay);
+        gameObject.GetComponent<BoxCollider>().enabled = true;
     }
 }
