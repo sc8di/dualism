760b71445f4075abe310c346e2d01081a6761b55
Shteelthefex <steel.fox621@gmail.com>
2020-05-05 13:59:44 +0300

    Routine Events added

---
+++ Assets/GhostBun/Scripts/GodzillaP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodzillaP : MonoBehaviour
+{
+    public float force = 500f;
+    [SerializeField]
+    ForceMode forceMode;
+
+    AllRigidbodies allrb;
+
+    private void Start()
+    {
+        allrb = FindObjectOfType<AllRigidbodies>();
+        gameObject.SetActive(false);
+    }
+
+    private void PushByStep()
+    {
+        
+        allrb.AddForceToAll(transform.up + Random.onUnitSphere * force, forceMode);
+    }
+}
