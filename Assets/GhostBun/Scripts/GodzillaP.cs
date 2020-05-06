065f7db47b32837c2099d47d2673721e4c248629
Shteelthefex <steel.fox621@gmail.com>
2020-05-06 19:03:49 +0300

    GodzillaCameraShake

--- Assets/GhostBun/Scripts/GodzillaP.cs
+++ Assets/GhostBun/Scripts/GodzillaP.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GodzillaP : MonoBehaviour
 {
     [SerializeField] private ForceMode _forceMode;
     [SerializeField] private AudioSource _stomp;
     private AllRigidbodies _allrb;
-    
+    [SerializeField] private float shakeCameraLength = 0.3f;
+
+    CinemachineBasicMultiChannelPerlin noise;
+
     public float force = 500f;
 
     private void Start()
     {
+        noise = FindObjectOfType<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         _allrb = FindObjectOfType<AllRigidbodies>();
         gameObject.SetActive(false);
     }
 
     private void PushByStep()
     {
+        StartCoroutine(ShakeCamera(shakeCameraLength));
         _allrb.AddForceToAll(transform.up + Random.onUnitSphere * force, _forceMode);
         _stomp.Play();
     }
+
+    private IEnumerator ShakeCamera(float seconds)
+    {
+        noise.m_AmplitudeGain = 3;
+        yield return new WaitForSeconds(seconds);
+        noise.m_AmplitudeGain = 0;
+    }
+
 
     private void DisableMe()
     {
